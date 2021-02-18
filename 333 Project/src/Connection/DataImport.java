0545b4f95d16af1d6a12bd532149e26a24a43759
ansarijrhit <57017656+ansarijrhit@users.noreply.github.com>
2021-02-18 17:07:02 -0500

    Theoretically imports are done (no testing done yet)

--- 333 Project/src/Connection/DataImport.java
+++ 333 Project/src/Connection/DataImport.java
@@ -10,9 +10,11 @@ import org.apache.poi.xssf.usermodel.*;
 public class DataImport {
 
 	private Connection con;
+	private UserService us;
 
-	public DataImport(Connection con) {
+	public DataImport(Connection con, UserService us) {
 		this.con = con;
+		this.us = us;
 	}
 
 	// 1st
@@ -32,8 +34,8 @@ public class DataImport {
 
 			con.setAutoCommit(false);
 
-			String sql = "INSERT INTO SpellTemp (Name, Description, [Cast Level]) VALUES (?, ?, ?)";
-			PreparedStatement statement = con.prepareStatement(sql);
+			String sql = "EXEC insert_spell @name = ?, @description = ?, @castLevel = ?";
+			CallableStatement statement = con.prepareCall(sql);
 
 			int count = 0;
 
@@ -113,8 +115,8 @@ public class DataImport {
 
 			con.setAutoCommit(false);
 
-			String sql = "INSERT INTO RaceTemp (Name, Description) VALUES (?, ?)";
-			PreparedStatement statement = con.prepareStatement(sql);
+			String sql = "EXEC insert_race @name = ?, @description = ?";
+			CallableStatement statement = con.prepareCall(sql);
 
 			int count = 0;
 
@@ -185,8 +187,8 @@ public class DataImport {
 
 			con.setAutoCommit(false);
 
-			String sql = "INSERT INTO ClassTemp (Name, Description) VALUES (?, ?)";
-			PreparedStatement statement = con.prepareStatement(sql);
+			String sql = "EXEC insert_class @className = ?, @description = ?";
+			CallableStatement statement = con.prepareCall(sql);
 
 			int count = 0;
 
@@ -256,8 +258,8 @@ public class DataImport {
 
 			con.setAutoCommit(false);
 
-			String sql = "INSERT INTO ClassCanCastTemp (ClassName, SpellName) VALUES (?, ?)";
-			PreparedStatement statement = con.prepareStatement(sql);
+			String sql = "EXEC insert_classCanCast @classname = ?, @spellName = ?";
+			CallableStatement statement = con.prepareCall(sql);
 
 			int count = 0;
 
@@ -270,7 +272,7 @@ public class DataImport {
 				while (cellIterator.hasNext()) {
 					Cell nextCell = cellIterator.next();
 
-					switch(i) {
+					switch (i) {
 					case 0:
 //						System.out.print("0 " + i + " ");
 						statement.setString(1, "Bard");
@@ -304,7 +306,7 @@ public class DataImport {
 						statement.setString(1, "Wizard");
 						break;
 					}
-					
+
 					int columnIndex = nextCell.getColumnIndex();
 
 					switch (columnIndex) {
@@ -343,9 +345,10 @@ public class DataImport {
 			System.out.println("Database error");
 			ex2.printStackTrace();
 		}
-		
+
 	}
 
+	//5th
 	public void importRaceCanCast(String filePath) {
 		int batchSize = 20;
 		try {
@@ -360,8 +363,8 @@ public class DataImport {
 
 			con.setAutoCommit(false);
 
-			String sql = "INSERT INTO RaceCanCastTemp (RaceName, SpellName) VALUES (?, ?)";
-			PreparedStatement statement = con.prepareStatement(sql);
+			String sql = "EXEC insert_raceCanCast @raceName = ?, @spellname = ?";
+			CallableStatement statement = con.prepareCall(sql);
 
 			int count = 0;
 
@@ -373,7 +376,7 @@ public class DataImport {
 
 				while (cellIterator.hasNext()) {
 					Cell nextCell = cellIterator.next();
-		
+
 					int columnIndex = nextCell.getColumnIndex();
 
 					switch (columnIndex) {
@@ -386,7 +389,6 @@ public class DataImport {
 						String spellName = nextCell.getStringCellValue();
 						statement.setString(2, spellName);
 					}
-						
 
 				}
 
@@ -416,11 +418,17 @@ public class DataImport {
 			System.out.println("Database error");
 			ex2.printStackTrace();
 		}
-		
+
 	}
-	
-	// 5th
-	public void importCampaign(String filePath) {
+
+	// 6th
+	// Excel file should include: Campaign name/description/DM Username.
+	// Locations (Name, Description)
+	// Characters (Name, Race): Major (Hitpoints, Alignment, Background,
+	// PlayerUsername, Class, Level),
+	// NPC (Occupation, LocationID)
+	// Notes (Name, Description)
+	public void importCampaign(String filePath, String name) {
 		int batchSize = 20;
 		try {
 			long start = System.currentTimeMillis();
@@ -429,47 +437,51 @@ public class DataImport {
 
 			Workbook workbook = new XSSFWorkbook(inputStream);
 
-			Sheet firstSheet = workbook.getSheetAt(0);
-			Iterator<Row> rowIterator = firstSheet.iterator();
+			Sheet playerSheet = workbook.getSheetAt(0);
+			Sheet locationSheet = workbook.getSheetAt(1);
+			Sheet majorCharacterSheet = workbook.getSheetAt(2);
+			Sheet NPCSheet = workbook.getSheetAt(3);
+			Sheet notesSheet = workbook.getSheetAt(4);
+			
+			importPlayers(playerSheet);
+			
+			Iterator<Row> rowIterator = playerSheet.iterator();
 
 			con.setAutoCommit(false);
 
 			String sql = "EXEC create_campaign @name = ?, @dm_username = ?";
 			CallableStatement statement = con.prepareCall(sql);
 
-			int count = 0;
-
 			rowIterator.next(); // skip the header row
 
-			while (rowIterator.hasNext()) {
-				Row nextRow = rowIterator.next();
-				Iterator<Cell> cellIterator = nextRow.cellIterator();
+			Row nextRow = rowIterator.next();
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
 
-				while (cellIterator.hasNext()) {
-					Cell nextCell = cellIterator.next();
+			Cell nextCell = cellIterator.next();
 
-					int columnIndex = nextCell.getColumnIndex();
+			int columnIndex = nextCell.getColumnIndex();
+			String dmUsername = "";
+			int campaignID = 0;
 
-					switch (columnIndex) {
-					case 0:
-						String name = nextCell.getStringCellValue();
-						statement.setString(1, name);
-						break;
-					case 1:
-						String dm_username = nextCell.getStringCellValue();
-						statement.setString(2, dm_username);
-					}
-
-				}
+			switch (columnIndex) {
+			case 0:
+				dmUsername = nextCell.getStringCellValue();
+				statement.setString(2, dmUsername);
 
 				statement.addBatch();
 
-				if (count % batchSize == 0) {
-					statement.executeBatch();
-				}
-
 			}
 
+			CallableStatement cs = con.prepareCall("{? = (SELECT dbo.getMostRecentCampaignID(?))}");
+			cs.setString(2, dmUsername);
+			cs.registerOutParameter(1, Types.INTEGER);
+			campaignID = cs.getInt(1);
+			
+			importLocations(locationSheet, dmUsername, campaignID);
+			importMajorCharacters(majorCharacterSheet, dmUsername, campaignID);
+			importNPCs(NPCSheet, dmUsername);
+			importNotes(notesSheet, dmUsername, campaignID);
+			
 			workbook.close();
 
 			// execute the remaining queries
@@ -488,5 +500,391 @@ public class DataImport {
 			System.out.println("Database error");
 			ex2.printStackTrace();
 		}
+	}
+
+	public void importPlayers(Sheet sheet) {
+		int batchSize = 20;
+		try {
+			long start = System.currentTimeMillis();
+
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			con.setAutoCommit(false);
+
+			
+			
+//			String sql = "EXEC create_campaign @name = ?, @dm_username = ?";
+//			CallableStatement statement = con.prepareCall(sql);
+
+			rowIterator.next(); // skip the header row
+
+			while (rowIterator.hasNext()) {
+				Row nextRow = rowIterator.next();
+				Iterator<Cell> cellIterator = nextRow.cellIterator();
+				
+				CallableStatement cs = con.prepareCall("{? = (SELECT dbo.playerExists(?))}");
+				
+				cs.registerOutParameter(1, Types.INTEGER);
+				int returnValue = cs.getInt(1);
+				boolean exists = false;
+				String username = "";
+				String name = "";
+
+				while (cellIterator.hasNext()) {
+					Cell nextCell = cellIterator.next();
+
+					int columnIndex = nextCell.getColumnIndex();
+
+					switch (columnIndex) {
+					case 0:
+						username = nextCell.getStringCellValue();
+						cs.setString(2, username);
+						cs.execute();
+						if(returnValue == 0) {
+							exists = true;
+						}
+						break;
+					case 1:
+						name = nextCell.getStringCellValue();
+						break;
+					case 2:
+						String password = nextCell.getStringCellValue();
+//						statement.setString(2, password);
+						if(!exists) {
+							us.register(username, name, password);
+						}
+					}
+					cs.execute();
+
+				}
+
+//				statement.addBatch();
+//
+//				if (count % batchSize == 0) {
+//					statement.executeBatch();
+//				}
+
+			}
+
+			// execute the remaining queries
+//			statement.executeBatch();
+
+			con.commit();
+//			con.close();
+
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done in %d ms\n", (end - start));
+
+		} catch (SQLException ex2) {
+			System.out.println("Database error");
+			ex2.printStackTrace();
+		}
+	}
+
+	public void importLocations(Sheet sheet, String dmUsername, int campaignID) {
+		int batchSize = 20;
+		try {
+			long start = System.currentTimeMillis();
+			
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			con.setAutoCommit(false);
+
+			String sql = "EXEC create_location @dm_username = ?, @name = ?, @description = ?, @campaignid = ?";
+			CallableStatement call = con.prepareCall(sql);
+
+			int count = 0;
+			
+			call.setString(1, dmUsername);
+			call.setInt(4, campaignID);
+			
+			rowIterator.next(); // skip the header row
+
+			while (rowIterator.hasNext()) {
+				Row nextRow = rowIterator.next();
+				Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+				while (cellIterator.hasNext()) {
+					Cell nextCell = cellIterator.next();
+
+					int columnIndex = nextCell.getColumnIndex();
+
+					switch (columnIndex) {
+					case 0:
+						String name = nextCell.getStringCellValue();
+						call.setString(2, name);
+						break;
+					case 1:
+						String description = nextCell.getStringCellValue();
+						call.setString(3, description);
+					}
+
+				}
+
+				call.addBatch();
+
+				if (count % batchSize == 0) {
+					call.executeBatch();
+				}
+
+			}
+
+
+			// execute the remaining queries
+			call.executeBatch();
+
+			con.commit();
+//			con.close();
+
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done in %d ms\n", (end - start));
+
+		}  catch (SQLException ex2) {
+			System.out.println("Database error");
+			ex2.printStackTrace();
+		}
+	}
+	
+	public void importMajorCharacters(Sheet sheet, String dmUsername, int campaignID) {
+		int batchSize = 20;
+		try {
+			long start = System.currentTimeMillis();
+
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			con.setAutoCommit(false);
+
+			String sql = "EXEC create_major_character @name = ?, @racename = ?, @hitpoints = ?, @alignment = ?, @background = ?, @class = ?, @level = ?, @campaign_id = ?";
+			
+
+			int count = 0;
+
+			rowIterator.next(); // skip the header row
+
+			while (rowIterator.hasNext()) {
+				Row nextRow = rowIterator.next();
+				Iterator<Cell> cellIterator = nextRow.cellIterator();
+				
+				String name = "";
+				String raceName = "";
+				int hitpoints = 0;
+				String alignment = "";
+				String background = "";
+				String username = "";
+				String pClass = "";
+				int level = 0;
+				while (cellIterator.hasNext()) {
+					Cell nextCell = cellIterator.next();
+
+					int columnIndex = nextCell.getColumnIndex();
+
+					switch (columnIndex) {
+					case 0:
+						name = nextCell.getStringCellValue();
+//						statement.setString(1, name);
+						break;
+					case 1:
+						raceName = nextCell.getStringCellValue();
+//						statement.setString(2, raceName);
+						break;
+					case 2:
+						hitpoints = (int) nextCell.getNumericCellValue();
+//						statement.setInt(3, hitpoints);
+						break;
+					case 3:
+						alignment = nextCell.getStringCellValue();
+//						statement.setString(4, alignment);
+						break;
+					case 4:
+						background = nextCell.getStringCellValue();
+//						statement.setString(5, background);
+						break;
+					case 5:
+						username = nextCell.getStringCellValue();
+						if(username.isBlank()) {
+							sql += ", @dm_username = ?";
+							username = dmUsername;
+						}
+						else {
+							sql += ", @playerusername = ?";
+						}
+						break;
+					case 6:
+						pClass = nextCell.getStringCellValue();
+						break;
+					case 7:
+						level = (int) nextCell.getNumericCellValue();
+					}
+				}
+				
+				CallableStatement statement = con.prepareCall(sql);
+				statement.setString(1, name);
+				statement.setString(2, raceName);
+				statement.setInt(3, hitpoints);
+				statement.setString(4, alignment);
+				statement.setString(5, background);
+				statement.setString(6, pClass);
+				statement.setInt(7, level);
+				statement.setString(8, username);
+				
+				statement.execute();
+//				statement.addBatch();
+
+//				if (count % batchSize == 0) {
+//					statement.executeBatch();
+//				}
+
+			}
+
+			// execute the remaining queries
+//			statement.executeBatch();
+
+			con.commit();
+//			con.close();
+
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done in %d ms\n", (end - start));
+
+		} catch (SQLException ex2) {
+			System.out.println("Database error");
+			ex2.printStackTrace();
+		}
+	}
+
+	public void importNPCs(Sheet sheet, String dmUsername) {
+		int batchSize = 20;
+		try {
+			long start = System.currentTimeMillis();
+
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			con.setAutoCommit(false);
+
+			String sql = "EXEC create_npc @dm_username = ?, @locationid = ?, "
+					+ "@name = ?, @racename = ?, @occupation = ?";
+			CallableStatement statement = con.prepareCall(sql);
+			
+			statement.setString(1, dmUsername);
+
+			int count = 0;
+
+			rowIterator.next(); // skip the header row
+
+			while (rowIterator.hasNext()) {
+				Row nextRow = rowIterator.next();
+				Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+				while (cellIterator.hasNext()) {
+					Cell nextCell = cellIterator.next();
+
+					int columnIndex = nextCell.getColumnIndex();
+
+					switch (columnIndex) {
+					case 0:
+						String name = nextCell.getStringCellValue();
+						statement.setString(3, name);
+						break;
+					case 1:
+						String race = nextCell.getStringCellValue();
+						statement.setString(4, race);
+						break;
+					case 2:
+						String occupation = nextCell.getStringCellValue();
+						statement.setString(5, occupation);
+						break;
+					case 3:
+						String locationName = nextCell.getStringCellValue();
+//						statement.setInt(2, locationID);
+						
+					}
+
+				}
+
+				statement.addBatch();
+
+				if (count % batchSize == 0) {
+					statement.executeBatch();
+				}
+
+			}
+
+			// execute the remaining queries
+			statement.executeBatch();
+
+			con.commit();
+//			con.close();
+
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done in %d ms\n", (end - start));
+
+		}  catch (SQLException ex2) {
+			System.out.println("Database error");
+			ex2.printStackTrace();
+		}
+	}
+
+	public void importNotes(Sheet sheet, String dmUsername, int campaignID) {
+		int batchSize = 20;
+		try {
+			long start = System.currentTimeMillis();
+
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			con.setAutoCommit(false);
+
+			String sql = "EXEC create_notes @dm_username = ?, @name = ?, @description = ?, @campaignid = ?";
+			CallableStatement statement = con.prepareCall(sql);
+			
+			statement.setString(1, dmUsername);
+			statement.setInt(4, campaignID);
+
+			int count = 0;
+
+			rowIterator.next(); // skip the header row
+
+			while (rowIterator.hasNext()) {
+				Row nextRow = rowIterator.next();
+				Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+				while (cellIterator.hasNext()) {
+					Cell nextCell = cellIterator.next();
+
+					int columnIndex = nextCell.getColumnIndex();
+
+					switch (columnIndex) {
+					case 0:
+						String name = nextCell.getStringCellValue();
+						statement.setString(2, name);
+						break;
+					case 1:
+						String description = nextCell.getStringCellValue();
+						statement.setString(3, description);
+						break;
+					}
+
+				}
+
+				statement.addBatch();
+
+				if (count % batchSize == 0) {
+					statement.executeBatch();
+				}
+
+			}
+
+			// execute the remaining queries
+			statement.executeBatch();
+
+			con.commit();
+//			con.close();
+
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done in %d ms\n", (end - start));
+
+		}  catch (SQLException ex2) {
+			System.out.println("Database error");
+			ex2.printStackTrace();
+		}
+		
 	}
 }
